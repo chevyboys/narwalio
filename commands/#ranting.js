3c41109b2db82a3a66e8aec839c7e5b9523b75e3
chevyboys <volcanicchevy@gmail.com>
2021-07-24 23:31:20 -0600

    lotsa bug fix

--- commands/#ranting.js
+++ commands/#ranting.js
@@ -6,24 +6,34 @@ let rantingChannel = "708140469527379978";
 let dungeonGuild = "639630243111501834";
 const flaggedWords = require('../config/rantingFlaggedWords.json');
 let updateConcernScore = async (user, change) => {
+    u.log("updating concern score");
     if (!change) return;
     change = change + 0;
-    let currentConcern = (await Module.db.user.fetchUser(user.id)).concernScore;
+    let currentConcern = (await Module.db.user.fetchUser(user)).concernScore;
     await Module.db.user.update(user, { concernScore: (change + currentConcern) });
 }
 
-async function normalizeConcern(reductionPercent) {
+async function normalizeConcern(reductionPercent, msg) {
     let users = (await Module.db.user.getUsers({ concernScore: { $gt: 0 } }));
+    if (msg) {
+        users = users.filter(u => {
+            return (msg.guild.members.cache.has(u.discordId) ? !(msg.guild.members.cache.get(u.discordId).user.bot) : false);
+        });
+    }
     let concernLog = [];
     let embed = u.embed().setColor("#a1fdff").setTitle("User Concern Levels This hour");
-
+    let channel;
+    let EtuID = "854552593509253150";
+    let NarwalioID = "637030744610439176"
+    if (Module.client.user.id == EtuID) channel = await Module.client.channels.cache.get("868684629857669130");
+    else channel = await Module.client.channels.cache.get("868685481204940800");
     let i = 0;
     for (user of users) {
-        embed.addField((Module.client.users.cache.get(user.discordId)).username, `Total: \`${user.concernScore}\``);
+        embed.addField((channel.guild.members.cache.has(user.discordId) ? (channel.guild.members.cache.get(user.discordId).displayName || Module.client.users.cache.get(user.discordId).username) : Module.client.users.cache.get(user.discordId).username), `Total: \`${user.concernScore}\``);
         if (i == 20) {
             try {
-                (await Module.client.channels.cache.get("868684629857669130")).send({ embed });
-                (await Module.client.channels.cache.get("868685481204940800")).send({ embed });
+                channel.send({ embed });
+
             } catch (e) {
                 u.log("Concern Error:" + e.toString());
                 return;
@@ -32,11 +42,11 @@ async function normalizeConcern(reductionPercent) {
             i = 0;
         }
         i++;
-        if(reductionPercent > 0) updateConcernScore(user, 0 - (user.concernScore * (reductionPercent || 5)/100));
+        if (reductionPercent > 0) updateConcernScore(user, 0 - (user.concernScore < 1 ? user.concernScore : user.concernScore * (reductionPercent || 5) / 100));
     }
     try {
-        (await Module.client.channels.cache.get("868684629857669130")).send({ embed });
-        (await Module.client.channels.cache.get("868685481204940800")).send({ embed });
+        channel.send({ embed });
+
     } catch (e) {
         u.log("Concern Error:" + e.toString());
         return;
@@ -47,7 +57,7 @@ Module
     .addCommand({
         name: "concern", // required, must be one word, all lower case
         description: "[add/subtract/set/summary] [@target]\n - add/subtract (+/-): adds or removes concern levels\n - set (=): overrides the current concern level\n - summary (all):sends a summary of all current concern levels to the admins of the primary servers", // recommended
-        permissions: (msg) => msg.guild.id == dungeonGuild && config.adminId.includes(msg.author.id), // optional, command will onnly run if this evaluate to true
+        permissions: (msg) => config.adminId.includes(msg.author.id), // optional, command will onnly run if this evaluate to true
         process: async (msg, suffix) => {
             u.preCommand(msg)
             //Your code here
@@ -56,8 +66,8 @@ Module
             })
 
             let args = [].concat(suffix.replace(/<+.*>\s*/gm, "").trim().split(" "));
-            
-            
+
+
             let change;
             switch (args[0].toLowerCase().trim()) {
                 //subtract from the current value
@@ -70,6 +80,7 @@ Module
                     }
                     change = parseInt(args[1].trim());
                     await updateConcernScore(target, (0 - change));
+                    msg.react(`➖`);
                     break;
                 //add to the current value
                 case "+":
@@ -81,8 +92,12 @@ Module
                     }
                     change = parseInt(args[1].trim());
                     await updateConcernScore(target, change);
+                    msg.react(`➕`);
                     break;
-                //set to a specific number    
+                case "summary":
+                case "all": normalizeConcern(0, msg);
+                    break;
+                //set to a specific number 
                 case "=":
                 case "set":
                 case "reset":
@@ -90,13 +105,10 @@ Module
                         return msg.channel.send("You need to tell me how much you want to change someone's concern score by");
                     }
                     change = parseInt(args[1].trim());
-                    await Module.db.user.update(member.user, { concernScore: change });
-                    break;
-                case "summary":
-                case "all": normilizeConcern(0);
-                    break;
+                    await Module.db.user.update(target.user, { concernScore: change });
                 default:
-                    targetConcernLevel = await Module.db.user.fetchUser(member.user.id).concernScore;
+                    targetConcernLevel = [].concat(await Module.db.user.getUsers({ discordId: target.user.id }));
+                    targetConcernLevel = targetConcernLevel[0].concernScore;
                     msg.channel.send("Current Concern Score for " + target.displayName + " is: `" + targetConcernLevel + "`");
                     break;
             }
@@ -111,23 +123,23 @@ Module
         }
         //Weights
         const weights = {
-            severeWords: 2,
-            moderateWords: 1.5,
-            minorWords: 1,
+            severeWords: (msg.channel.id == rantingChannel) ? 3 : 2,
+            moderateWords: (msg.channel.id == rantingChannel) ? 1.5 : 1,
+            minorWords: (msg.channel.id == rantingChannel) ? 0.75 : 0.5,
             rantingMessageLength: 0.1
         }
         //increase concern for ranting
         let args = msg.content.trim().toLowerCase().split(" ");
 
         function getConcernPoints(searchString, flaggedWordsArray, weight) {
-            return weight * (searchString.split(' ').map(messageWords => flaggedWordsArray.filter(flaggedWords => messageWords.startsWith(flaggedWords)).length).reduce((messageWords, flaggedWords) => messageWords + flaggedWords, 0));
+            return weight * (searchString.split(' ').map(messageWord => (flaggedWordsArray.filter(flaggedWord => (messageWord.startsWith(flaggedWord))).length)).reduce((messageWord, flaggedWord) => messageWord + flaggedWord, 0));
         };
         let total =
             getConcernPoints(msg.content, flaggedWords.severeWords, weights.severeWords)
             + getConcernPoints(msg.content, flaggedWords.moderateWords, weights.moderateWords)
-            + getConcernPoints(msg.content, flaggedWords.minorWords)
-            + ((msg.channel.id == rantingChannel) ? (args.length * weights.rantingMessageLength) : 0);
-        if(total > 0) {
+            + getConcernPoints(msg.content, flaggedWords.minorWords, weights.minorWords);
+        + ((msg.channel.id == rantingChannel) ? (args.length * weights.rantingMessageLength) : 0);
+        if (total > 0) {
             await updateConcernScore(msg.author, total);
             console.log(`Concern for ${msg.member.displayname} increased by: ${total}`);
         }
@@ -138,6 +150,6 @@ Module
 Module.setClockwork(() => {
     try {
         return setInterval(normalizeConcern, 60 * 60 * 1000);
-    } catch (e) { u.errorHandler(e, "Normilize Concern Clockwork Error"); }
+    } catch (e) { u.errorHandler(e, "Normalize Concern Clockwork Error"); }
 });
 module.exports = Module;
